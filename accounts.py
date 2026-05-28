00bc9f31e84ed26fccd329e2ae39534766ab9aea
tjdwlsl8888 <tjdwlsl8888@gmail.com>
2026-05-28 12:17:03 +0900

    20260528 branchtest

---
+++ accounts.py
@@ -0,0 +1,47 @@
+# - bank account
+#     - 회원 1인당 1개의 계좌만 관리 
+#     - 회원 1인당 N개의 계좌만 관리
+#     - 입/출금 내역 
+
+import UserSystem
+
+
+UserSystem. 
+
+
+userId = int(input('ID:'))
+accountNum = int(input('계좌번호:'))
+
+class Account:
+   
+    accountsDict = {}
+    userBalence = []
+
+    def __init__(self, id, account, balance):
+        self.key = id 
+        self.userdata1 = account
+        self.userdata2 = balance
+       
+        Account.accountsDict[id] = account
+        userBalence.append(balance)
+
+    def print1(self):
+    
+        if accountNum in accountsDict:
+            return '계좌가 이미 있습니다.'
+        else:
+            return '계좌가 없습니다.'
+        
+
+    def Deposit(self):
+        userBalence
+    
+    def Withdrawal(self):   
+
+
+    def imp    
+
+    def manageAccount(self):
+        
+
+
